6d64bc1718c8e3d3b210269465cb31755f1b698e
mssoheil88 <mssoheil88@yahoo.com>
2019-10-07 11:09:59 +0330

    feat: formHelper slot

--- src/components/formHelper/formHelper.vue
+++ src/components/formHelper/formHelper.vue
@@ -1,9 +1,30 @@
 <template>
-	<div id="ninjas">
-		<slot name="title" />
-		<h1>I am the form helper</h1>
-		<!-- it's somehow like the children in react -->
-		<slot name="text" />
+	<div>
+		<h1>Please fill the form...</h1>
+		<form>
+			<div id="form-header">
+				<slot name="form-header" />
+			</div>
+			<div id="form-fields">
+				<slot name="form-fields" />
+			</div>
+			<div id="form-controls">
+				<slot name="form-controls" />
+			</div>
+			<div id="useful-links">
+				<ul>
+					<li>
+						<a href="#">Link 1</a>
+					</li>
+					<li>
+						<a href="#">Link 2</a>
+					</li>
+					<li>
+						<a href="#">Link 3</a>
+					</li>
+				</ul>
+			</div>
+		</form>
 	</div>
 </template>
 
