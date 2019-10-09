6af9b042b2da3df87a5e0a84ef8d50f512cd793e
mssoheil88 <mssoheil88@yahoo.com>
2019-10-09 10:00:50 +0330

    feat(#35): custom directive

--- src/components/showBlogs/showBlogs.vue
+++ src/components/showBlogs/showBlogs.vue
@@ -1,8 +1,8 @@
 <template>
-	<div id="show-blogs">
+	<div v-theme:column="'narrow'" id="show-blogs">
 		<h1>All blog articles</h1>
 		<div v-for="blog in blogs" class="single-blog" :key="blog.id">
-			<h2>{{ blog.title }}</h2>
+			<h2 v-rainbow>{{ blog.title }}</h2>
 			<article>{{ blog.body }}</article>
 		</div>
 	</div>
--- src/main.js
+++ src/main.js
@@ -10,6 +10,33 @@ export const bus = new Vue();
 
 Vue.use(VueResource);
 
+// custom directives
+Vue.directive("rainbow", {
+	// vnode -> virtual node
+	// binding -> value passed
+	// el -> element itself
+	bind(el, binding, vnode) {
+		console.log(parseInt(Math.random() * 255));
+		const colorCode = () => parseInt(Math.random() * 255);
+		el.style.color = `rgb(${colorCode()},${colorCode()},${colorCode()})`;
+	},
+});
+
+Vue.directive("theme", {
+	bind(el, binding, vnode) {
+		const { value, arg } = binding;
+		if (value === "wide") {
+			el.style.maxWidth = "1200px";
+		} else if (value === "narrow") {
+			el.style.maxWidth = "560px";
+		}
+		if (arg === "column") {
+			el.style.background = "#ddd";
+			el.style.padding = "20px";
+		}
+	},
+});
+
 new Vue({
 	render: h => h(App),
 }).$mount("#app");
