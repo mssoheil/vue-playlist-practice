cdf09f413a893a2d820c6f30ac724274bfe79bf7
mssoheil <mssoheil88@yahoo.com>
2019-10-11 12:39:26 +0330

    feat: add name to routes

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,20 @@ import ShowBlogs from "./screens/showBlogs/showBlogs";
 import AddBlog from "./screens/addBlog/addBlog";
 import SingleBlog from "./screens/singleBlog/singleBlog";
 
+
 export default [{
 		path: "/",
+		name: "home",
 		component: ShowBlogs
 	},
 	{
 		path: "/add",
+		name: "addBlog",
 		component: AddBlog
 	},
 	{
 		path: "/blog/:id",
+		name: "singleBlog",
 		component: SingleBlog
 	},
 ]
